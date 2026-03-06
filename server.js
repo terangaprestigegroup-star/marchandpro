dcb38bbb99083d3692d773e8b1ae2223433b6572
terangaprestigegroup-star <terangaprestigegroup@gmail.com>
2026-03-06 09:59:53 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require('express');const cors = require('cors');
 const path = require('path');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const twilio = require('twilio');
 
-const app = express();
+const app = express();app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
